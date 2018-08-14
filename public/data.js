f6090ba34e9452d93a9b43e0ae7a436c05ad3604
Letícia Faria <leticianfaria@hotmail.com>
2018-08-14 17:45:45 -0300

    Update

---
+++ public/data.js
@@ -0,0 +1,161 @@
+var data = [{
+  "wine": "Scarecrow, Cabernet Sauvignon, Rutherford",
+  "regions": [
+      "California"
+  ],
+  "country": "Usa",
+  "score": 98.05,
+},
+{
+  "wine": "Bond Estates, Pluribus Red, Napa Valley",
+  "regions": [
+      "California"
+  ],
+  "country": "Usa",
+  "score": 96.52
+},
+{
+  "wine": "Harlan Estate, Napa Valley",
+  "regions": [
+      "California"
+  ],
+  "country": "Usa",
+  "score": 96.03
+},
+{
+  "wine": "M. Chapoutier, Les Greffieux Rouge, Hermitage",
+  "regions": [
+      "Rhone"
+  ],
+  "country": "France",
+  "score": 95.57
+},
+{
+  "wine": "Bond Estates, Vecina Red, Napa Valley",
+  "regions": [
+      "California"
+  ],
+  "country": "Usa",
+  "score": 95.54
+},
+{
+  "wine": "Bond Estates, St Eden Red, Napa Valley",
+  "regions": [
+      "California"
+  ],
+  "country": "Usa",
+  "score": 95.44
+},
+{
+  "wine": "M. Chapoutier, Les Granits, Saint Joseph",
+  "regions": [
+      "Rhone"
+  ],
+  "country": "France",
+  "score": 94.93
+},
+{
+  "wine": "M. Chapoutier, Barbe Rac, Chateauneuf Du Pape",
+  "regions": [
+      "Rhone"
+  ],
+  "country": "France",
+  "score": 94.39
+},
+{
+  "wine": "M. Chapoutier, Crozes-Ermitage Les Varonniers, Hermitage",
+  "regions": [
+      "Rhone"
+  ],
+  "country": "France",
+  "score": 94.11
+},
+{
+  "wine": "Betz Family Winery, La Cote Rousse Syrah, Red Mountain",
+  "regions": [
+      "Washington"
+  ],
+  "country": "Usa",
+  "score": 93.29
+},
+{
+  "wine": "M. Chapoutier, Croix De Bois Rouge, Chateauneuf Du Pape",
+  "regions": [
+      "Rhone"
+  ],
+  "country": "France",
+  "score": 92.5
+},
+{
+  "wine": "Numanthia, Termes, Toro",
+  "regions": [
+      "Spain"
+  ],
+  "country": "Spain",
+  "score": 92.13
+},
+{
+  "wine": "M. Chapoutier, Les Granits Blanc, Blanc, Saint Joseph",
+  "regions": [
+      "Rhone"
+  ],
+  "country": "France",
+  "score": 92.09
+},
+{
+  "wine": "Peter Michael Winery, L'Apres-Midi Sauvignon Blanc, White, Knights Valley",
+  "regions": [
+      "California"
+  ],
+  "country": "Usa",
+  "score": 91.45
+},
+{
+  "wine": "Ornellaia, Bolgheri",
+  "regions": [
+      "Tuscany"
+  ],
+  "country": "Italy",
+  "score": 98.14
+},
+{
+  "wine": "Kongsgaard, The Judge Chardonnay, White, Napa Valley",
+  "regions": [
+      "California"
+  ],
+  "country": "Usa",
+  "score": 97.03
+},
+{
+  "wine": "Aubert Wines, Larry Hyde & Sons Vineyard Chardonnay, White, Carneros",
+  "regions": [
+      "California"
+  ],
+  "country": "Usa",
+  "score": 96.81
+},
+{
+  "wine": "Martinelli, Jackass Vineyard Zinfandel, Russian River Valley",
+  "regions": [
+      "California"
+  ],
+  "country": "Usa",
+  "score": 96.18
+},
+{
+  "wine": "Tenuta San Guido, Sassicaia, Bolgheri Sassicaia",
+  "regions": [
+      "Tuscany"
+  ],
+  "country": "Italy",
+  "score": 96.09
+},
+{
+  "wine": "Biserno, Tenuta di Biserno, Lodovico, Toscana",
+  "regions": [
+      "Tuscany"
+  ],
+  "country": "Italy",
+  "score": 95.51
+}
+]
